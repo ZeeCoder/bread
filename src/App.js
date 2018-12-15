8a0fd3c882dfe658b07e6264fdd9fdfc8af2c2b2
Viktor Hubert <rpgmorpheus@gmail.com>
2018-12-15 23:18:33 +0000

    change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 6
+            Learn React 7
           </a>
         </header>
       </div>
